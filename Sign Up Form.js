bab892261c5dfcdb7db55993d9cfb9a84a12ecca
Reza zhyani <116823598+imalirezai@users.noreply.github.com>
2024-01-25 14:59:37 +0330

    Add files via upload

---
+++ Sign Up Form.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+const signupBtn = document.querySelector(".green-bg button");
+
+
+signupBtn.addEventListener('click',
+()=>{
+    container.classList.toggle("change")
+})
